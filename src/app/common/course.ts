70ee10f2beb0fc78063c47334cdb9a40d78c185c
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-22 11:44:24 -0700

    Updates to course model and various html

--- src/app/common/course.ts
+++ src/app/common/course.ts
@@ -1,9 +1,33 @@
 export class Course {
+  id: number;
+  user_id: number;
+  title: string;
+  company_id: number;
+  price: number;
+  date_purchased: number;
+
   constructor(
-    public user_id: number,
-    public title: string,
-    public company_id: number,
-    public price: number,
-    public date_purchased: number
-  ) {}
+    id: number,
+    user_id: number,
+    title: string,
+    company_id: number,
+    price: number,
+    date_purchased: number
+  ) {
+    (this.id = id),
+      (this.user_id = user_id),
+      (this.company_id = company_id),
+      (this.title = title),
+      (this.price = price),
+      (this.date_purchased = date_purchased);
+  }
+
+  // constructor(
+  //   public id: number,
+  //   public user_id: number,
+  //   public title: string,
+  //   public company_id: number,
+  //   public price: number,
+  //   public date_purchased: number
+  // ) {}
 }
